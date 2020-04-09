c7f3eb7fd2a635080a8a018fa713749e7ec28f3c
Mahendra AR <mahendra@arraigntech.com>
2020-04-10 02:05:42 +0530

    Create gh-pages branch with static content

---
+++ build/precache-manifest.bb11a0fdb42096ee005b4fe90121e3d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625987003796aab21b0706dfc3294445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c09776c4008105e541c1",
+    "url": "/static/css/main.8704b84d.chunk.css"
+  },
+  {
+    "revision": "107f018adeb6885286ec",
+    "url": "/static/js/2.bcf5b1d7.chunk.js"
+  },
+  {
+    "revision": "c09776c4008105e541c1",
+    "url": "/static/js/main.129fd383.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
