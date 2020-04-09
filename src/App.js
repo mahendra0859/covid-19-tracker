6bfbbd9c62e53206216d4146bc56832c02ead48c
Mahendra A R <30443523+mahendra0859@users.noreply.github.com>
2020-04-10 01:50:22 +0530

    Charts added
    Bar and line charts and other api added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,30 @@ import React from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api/index";
+import coronaImage from "./images/image.png";
 
 class App extends React.Component {
   state = {
-    data: {}
+    data: {},
+    country: ""
   };
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
-    console.log("this", this.state);
   }
+  handleCountryChange = async country => {
+    console.log("country", country);
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country });
+  };
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img src={coronaImage} alt="COVID - 19" className={styles.image} />
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
